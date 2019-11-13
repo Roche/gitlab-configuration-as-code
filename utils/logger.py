0c53dd280230a68b45d4d08df466cb45ab8752cb
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2019-11-13 21:24:53 +0100

    added dedicated logger

---
+++ utils/logger.py
@@ -0,0 +1,46 @@
+import logging
+
+logging.basicConfig(format='[%(levelname)s] [%(name)s] %(message)s', level=logging.DEBUG)
+
+
+def get_logger(name='gcasc'):
+    return Logger(name)
+
+
+def _apply_masking(value, mask=False):
+    return '***' if mask else value
+
+
+class Logger(object):
+
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+
+    def log_update(self, field, old_value, new_value, masked=False):
+        self.debug('Changed value of %s from %s to %s', field, _apply_masking(old_value, masked),
+                   _apply_masking(new_value, masked))
+        pass
+
+    def log_create(self, value, field=None, masked=False):
+        value_to_log = _apply_masking(value, masked)
+        if field is None:
+            self.debug('Created new object: %s', value_to_log)
+        else:
+            self.debug('Created new field %s with value %s', field, value_to_log)
+        pass
+
+    def log_delete(self, field):
+        self.debug('Deleted field or object: %s', field)
+        pass
+
+    def debug(self, message, *args):
+        self.logger.debug(message, *args)
+
+    def error(self, message, *args):
+        self.logger.error(message, *args)
+
+    def warn(self, message, *args):
+        self.logger.warning(message, *args)
+
+    def info(self, message, *args):
+        self.logger.info(message, *args)
