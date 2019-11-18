ff2257a8a7cd25afb4feb878ba7e8e70415ab52e
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2019-11-18 16:45:15 +0100

    helper to read yaml from string

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,7 +1,10 @@
 import os
 
 import yaml
-
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 def __context():
     dir = os.getcwd()
@@ -23,3 +26,7 @@ def read_yaml(file):
     with open('{0}/data/{1}'.format(CONTEXT, file)) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     return data
+
+
+def read_yaml_from_string(str):
+    return yaml.load(StringIO(str), Loader=yaml.FullLoader)
