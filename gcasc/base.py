8e0aaea39abb4d8117604adf72c8bb960b54b99d
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2019-11-18 15:01:43 +0100

    added license configurer

--- gcasc/base.py
+++ gcasc/base.py
@@ -1,28 +1,69 @@
 from abc import ABC
 from enum import Enum
 
+from gitlab import Gitlab
+
 
 class Mode(Enum):
+    TEST_SKIP_VALIDATION = -2
     TEST = -1
     APPLY = 0
 
 
 class Configurer(ABC):
-    def __init__(self, gitlab, config, mode=Mode.APPLY):
-        # type: (gitlab.Gitlab, dict, Mode)->any
+    def __init__(self, gitlab, config, mode=Mode.APPLY):  # type: (Gitlab, dict, Mode)->any
         self.gitlab = gitlab
         self.config = config
         self.mode = mode
-        self._validate()
+        if self.mode != Mode.TEST_SKIP_VALIDATION:
+            self._validate()
 
     def configure(self):
         pass
 
-    def validate(self):
+    def validate(self):  # type: () -> ValidationResult
         pass
 
     def _validate(self):
         if self.gitlab is None:
             raise RuntimeError("GitLab client is not provided")
         if self.config is not None:
-            self.validate()
+            result = self.validate()
+            if result.has_errors():
+                error = ValidationError.from_validation_result(result)
+                raise error
+
+
+class ValidationResult(object):
+
+    def __init__(self, errors=None):
+        self.errors = [] if errors is None else errors
+
+    def add(self, error, *args):  # type: (str, *str) -> ()
+        message = error.format(*args) if args else error
+        self.errors.append(message)
+
+    def get(self):
+        return self.errors
+
+    def get_as_string(self):
+        return '\n'.join(self.errors)
+
+    def has_errors(self):
+        return self.errors.__len__() > 0
+
+    def has_error(self, error):
+        for e in self.errors:
+            if error in e:
+                return True
+        return False
+
+
+class ValidationError(RuntimeError):
+
+    @staticmethod
+    def from_validation_result(validation_result):
+        if validation_result is None:
+            return ValidationError('Validation failed, but no errors provided')
+        errors = validation_result.get_as_string()
+        return ValidationError('Configuration validation failed with errors:\n{0}'.format(errors))
---
+++ gcasc/license.py
@@ -0,0 +1,78 @@
+import datetime
+
+import gitlab
+
+from .utils import logger
+import gcasc.utils.validators as validators
+
+from .base import Configurer, Mode, ValidationResult
+
+logger = logger.get_logger("configurer.license")
+
+
+class LicenseConfigurer(Configurer):
+    def __init__(self, gitlab, license, mode=Mode.APPLY): # type: (gitlab.Gitlab, dict, Mode)->LicenseConfigurer
+        super().__init__(gitlab, license, mode=mode)
+
+    def configure(self):
+        logger.info("Configuring GitLab licenses")
+        license = self.gitlab.get_license()
+        if not self.__check_if_same_license(license) and self.mode == Mode.APPLY:
+            license = self._update_license()
+        logger.info(
+            "Current license:\nplan: %s\nstarts_at: %s\nexpires_at: %s\nuser_limit: %s",
+            self.__get_plan(license),
+            self.__get_starts_at(license),
+            self.__get_expires_at(license),
+            self.__get_user_limit(license)
+        )
+        return license
+
+    def __get(self, field, lic=None):  # type: (str, dict) -> str
+        license_config = lic if lic is not None else self.config
+        return license_config.get(field)
+
+    def __get_starts_at(self, license=None):  # type: (dict) -> str
+        starts_at = self.__get('starts_at', license)
+        s_at = starts_at.strftime("%Y-%m-%d") if isinstance(starts_at, datetime.date) else starts_at
+        return s_at
+
+    def __get_plan(self, license=None):  # type: (dict) -> str
+        return self.__get('plan', license)
+
+    def __get_expires_at(self, license=None):  # type: (dict) -> str
+        return self.__get('expires_at', license)
+
+    def __get_user_limit(self, license=None):  # type: (dict) -> str
+        return self.__get('user_limit', license)
+
+    def __get_data(self, license=None):  # type: (dict) -> str
+        return self.__get('data', license)
+
+    def __check_if_same_license(self, license):
+        return (
+                self.__get_starts_at() == self.__get_starts_at(license)
+                and self.__get_plan() == self.__get_plan(license)
+                and self.__get_user_limit() == self.__get_user_limit(license)
+        )
+
+    def _update_license(self):
+        logger.info("Updating GitLab license...")
+        return self.gitlab.set_license(self.__get_data())
+
+    def validate(self):  # type: () -> ValidationResult
+        errors = ValidationResult()
+        if not self.__get_starts_at():
+            errors.add("License must have starts_at property set in format yyyy-MM-dd")
+        elif not validators.validate_date(self.__get_starts_at(), "%Y-%m-%d"):
+            errors.add("starts_at license property must follow format yyyy-MM-dd, e.g. 2019-03-28")
+        if not self.__get_plan():
+            errors.add("License must have plan property set")
+        elif not self.__get_plan() in ["starter", "premium", "ultimate"]:
+            errors.add("License plan property was '{0}', but should be one of: starter, premium or ultimate",
+                       self.__get_plan())
+        if not self.__get_user_limit():
+            errors.add("License must have user_limit property set in format")
+        if not self.__get_data():
+            errors.add("License must have data property configured containing license itself")
+        return errors
