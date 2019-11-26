a222453c145b2abc92b0939d98a4a5e574c0d6d9
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2019-11-26 08:56:24 +0100

    define master toctree document

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,9 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 
+# The master toctree document.
+master_doc = "index"
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
